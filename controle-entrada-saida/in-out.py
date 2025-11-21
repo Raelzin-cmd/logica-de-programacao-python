3c29c3507fd313bf67b70a831e0c79a33b9331d0
Israel Almeida <almeidev.rael@gmail.com>
2025-11-21 16:48:31 -0300

    Adicionado exemplo de input

---
+++ controle-entrada-saida/in-out.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ') #input é um método
+idade = int(input('Idade: '))
+altura = float(input('Altura: '))
+
+#print é o output da operação
+print(f'Olá, me chamo {nome}! Tenho {idade} anos de idade e meço {altura}m.')
