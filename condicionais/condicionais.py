5e2ba39e19d58be80a8775dd56ec685e7169ab70
Israel Almeida <almeidev.rael@gmail.com>
2025-11-21 15:41:56 -0300

    Alteração nos prints

--- condicionais/condicionais.py
+++ condicionais/condicionais.py
@@ -1,10 +1,10 @@
 idade = 87
 
 if idade < 18:
-    print(f'Você tem {idade} anos. Você é menor')
+    print(f'Você tem {idade} anos. Você é menor!')
 elif idade >= 18 and idade < 30:
-    print(f'Você tem {idade} anos. Você é jovem')
+    print(f'Você tem {idade} anos. Você é jovem!')
 elif idade >= 30 and idade < 60:
-    print(f'Você tem {idade} anos. Você é adulto')
+    print(f'Você tem {idade} anos. Você é adulto!')
 else:
-    print(f'Você tem {idade} anos. Você é idoso')
+    print(f'Você tem {idade} anos. Você é idoso!')
