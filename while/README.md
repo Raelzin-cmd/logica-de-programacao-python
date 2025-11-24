488dc7921b341e0df2ed43de3714b436b72f4960
Israel Almeida <almeidev.rael@gmail.com>
2025-11-24 15:20:06 -0300

    Primeiro exemplo de while no python

---
+++ while/README.md
@@ -0,0 +1,6 @@
+# While
+
+O comando while é uma estrutura de repetição enquanto uma condição for verdadeira.
+
+"Enquanto algo for verdade, continue fazendo isso".
+
---
+++ while/while.py
@@ -0,0 +1,7 @@
+num = 0
+
+# O programa será encerrado quando a posição for igual ao num
+
+while num < 10:
+    print(num)
+    num += 1    # Adicionando um numero cada vez que o programa passar por aqui
