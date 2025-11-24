7f43dcbf5e8b2f8d1bbe1bc3fecaa3569ee8f9af
Israel Almeida <almeidev.rael@gmail.com>
2025-11-24 17:55:27 -0300

    Primeira função no python

---
+++ funcoes/funcoes.py
@@ -0,0 +1,11 @@
+'''
+def saudacao(pessoa):
+    print(f'Olá, seja bem-vindo! {pessoa}')
+
+saudacao('Rael')
+'''
+
+def saudacao(pessoa):
+    return f'Olá, seja bem-vindo {pessoa}'
+
+print(saudacao('Rael'))
