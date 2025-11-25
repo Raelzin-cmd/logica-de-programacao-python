14ba6d03a255e09354c54695136d446b8d05da94
Israel Almeida <almeidev.rael@gmail.com>
2025-11-25 17:36:46 -0300

    Exemplo de importação desestruturada

---
+++ modulos/modulo_calc.py
@@ -0,0 +1,18 @@
+# Importando seletivamente
+# from calculadora import soma, div
+
+
+'''
+Aqui estamos importando todos as funções do arquivo calculadora
+Evitando também de inicializar a função logo abaixo, enxugando o código
+'''
+
+# Importando todas as funções de forma desestruturada
+from calculadora import *
+print(soma(3, 5))
+
+print(div(3, 5))
+
+print(mult(3, 5))
+
+print(sub(3, 5))
